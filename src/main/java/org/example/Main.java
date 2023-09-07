1f5c8b0b3d16efdda24c924b82da99813b166d9f
orlanddoneto <neto020735@gmail.com>
2023-09-07 09:31:55 -0300

    tentativa de limpar a tela

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,15 +2,22 @@ package org.example;
 
 import org.example.Agents.ReactiveAgent;
 
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Field field = new Field();
         ReactiveAgent a = new ReactiveAgent("A", 0, 0);
         field.spawn(a);
+       
         while (true) {
+        	System.out.flush();
+        	System.out.print("\033[H\033[2J");
             a.decide(field);
             field.update();
             Thread.sleep(500);
+            System.out.flush();
+        	System.out.print("\033[H\033[2J");
+           
         }
 
     }
